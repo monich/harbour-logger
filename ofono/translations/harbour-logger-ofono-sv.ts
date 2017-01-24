74c0b47aa556f413a8560e956cabbed1d49e1a48
Åke Engelbrektson <eson57@users.noreply.github.com>
2017-01-24 06:36:10 +0100

    Create harbour-logger-ofono-sv.ts

---
+++ ofono/translations/harbour-logger-ofono-sv.ts
@@ -0,0 +1,130 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv">
+<context>
+    <name></name>
+    <message id="logger-cover-title">
+        <source>Log</source>
+        <translation>Logg</translation>
+    </message>
+    <message id="logger-mainpage-swipe-left-hint">
+        <source>Swipe left to configure log categories</source>
+        <translation>Svep vänster för att konfigurera loggkategorier</translation>
+    </message>
+    <message id="ofono-logger-pm-fix-mobile-data">
+        <source>Fix mobile data</source>
+        <extracomment>Pull-down menu item</extracomment>
+        <translation>Fixa mobildata</translation>
+    </message>
+    <message id="logger-logpage-pm-clear-log">
+        <source>Clear log</source>
+        <translation>Rensa loggen</translation>
+    </message>
+    <message id="logger-logpage-pm-pack-and-send">
+        <source>Pack and send</source>
+        <translation>Paketera och sänd</translation>
+    </message>
+    <message id="logger-logpage-title">
+        <source>Log</source>
+        <translation>Logg</translation>
+    </message>
+    <message id="logger-logpage-msg-connected">
+        <source>Connected.</source>
+        <translation>Ansluten.</translation>
+    </message>
+    <message id="logger-logpage-msg-connect-error">
+        <source>We are having trouble connecting to the service.</source>
+        <translation>Vi har problem med att ansluta till tjänsten.</translation>
+    </message>
+    <message id="logger-logpage-msg-disconnected">
+        <source>Connection lost.</source>
+        <translation>Anslutning avbröts.</translation>
+    </message>
+    <message id="logger-logpage-msg-log-cleared">
+        <source>Log cleared</source>
+        <translation>Loggen rensad</translation>
+    </message>
+    <message id="logger-categories-title">
+        <source>Log categories</source>
+        <translation>Loggkategorier</translation>
+    </message>
+    <message id="logger-categories-pm-enable-all">
+        <source>Enable all</source>
+        <translation>Aktivera alla</translation>
+    </message>
+    <message id="logger-categories-pm-disable-all">
+        <source>Disable all</source>
+        <translation>Inaktivera alla</translation>
+    </message>
+    <message id="logger-categories-pm-default">
+        <source>Reset to default</source>
+        <translation>Återställ standard</translation>
+    </message>
+    <message id="logger-categories-not-connected">
+        <source>Not connected</source>
+        <translation>Ej ansluten</translation>
+    </message>
+    <message id="logger-sharemethodlist-add-account">
+        <source>Add account</source>
+        <translation>Lägg till konto</translation>
+    </message>
+    <message id="logger-sharepage-save-ok">
+        <source>Saved %1</source>
+        <translation>Sparade %1</translation>
+    </message>
+    <message id="logger-sharepage-save-error">
+        <source>Failed to save %1</source>
+        <translation>Kunde inte spara %1</translation>
+    </message>
+    <message id="logger-sharepage-pm-save-to-documents">
+        <source>Save to documents</source>
+        <translation>Spara i Dokument</translation>
+    </message>
+    <message id="logger-sharepage-header">
+        <source>Pack and send</source>
+        <translation>Paketera och sänd</translation>
+    </message>
+    <message id="logger-sharepage-default-subject">
+        <source>Log</source>
+        <extracomment>Default email subject</extracomment>
+        <translation>Logg</translation>
+    </message>
+    <message id="logger-sharepage-default-email">
+        <source></source>
+        <extracomment>Default email recipient</extracomment>
+        <translation></translation>
+    </message>
+    <message id="logger-sharepage-warning">
+        <source>Keep in mind that some of the information contained in this archive may be considered private. If you would like to check what you are about to send, please consider sending it to yourself and checking its contents first.</source>
+        <translation>Notera att viss information i detta arkiv kan anses vara av privat natur. Om du vill veta vad du är på väg att skicka, kan du skicka det till dig själv och kontrollera innehållet först.</translation>
+    </message>
+    <message id="logger-sharepage-please-wait">
+        <source>Please wait</source>
+        <translation>Vänta</translation>
+    </message>
+    <message id="logger-settings-page-header">
+        <source>Logger</source>
+        <translation>Datalogger</translation>
+    </message>
+    <message id="logger-settings-section-header-display">
+        <source>Display</source>
+        <translation>Visa</translation>
+    </message>
+    <message id="logger-settings-logsizelimit">
+        <source>Screen buffer size</source>
+        <translation>Skärmbuffertstorlek</translation>
+    </message>
+    <message id="logger-settings-logsizelimit-description">
+        <source>Don&apos;t worry, everything will be written to the log file regardless of the screen buffer size.</source>
+        <translation>Oroa dig inte, allt kommer att skrivas till loggfilen oavsett skärmbuffertstorlek.</translation>
+    </message>
+    <message id="logger-settings-logsizelimit-unlimited">
+        <source>Unlimited</source>
+        <translation>Obegränsad</translation>
+    </message>
+    <message id="logger-settings-fontsize-label">
+        <source>Font size</source>
+        <translation>Teckenstorlek</translation>
+    </message>
+</context>
+</TS>
