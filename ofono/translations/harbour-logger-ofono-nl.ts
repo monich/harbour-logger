a667fb00278bdf30d01610a16a2e58969e2be5c3
d9h20f <>
2018-01-30 14:54:35 +0100

    Dutch translation

---
+++ ofono/translations/harbour-logger-ofono-nl.ts
@@ -0,0 +1,167 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="nl">
+<context>
+    <name></name>
+    <message id="logger-cover-title">
+        <source>Log</source>
+        <translation>ofono</translation>
+    </message>
+    <message id="logger-mainpage-swipe-left-hint">
+        <source>Swipe left to configure log categories</source>
+        <translation>Swipe left to configure log categories</translation>
+    </message>
+    <message id="ofono-logger-pm-fix-mobile-data">
+        <source>Fix mobile data</source>
+        <extracomment>Pull-down menu item</extracomment>
+        <translation>Mobiele gegevens herstellen</translation>
+    </message>
+    <message id="ofono-logger-pm-enable-mobile-data">
+        <source>Enable mobile data</source>
+        <extracomment>Pull-down menu item</extracomment>
+        <translation>Mobiele gegevens inschakelen</translation>
+    </message>
+    <message id="logger-logpage-pm-clear-log">
+        <source>Clear log</source>
+        <translation>Log wissen</translation>
+    </message>
+    <message id="logger-logpage-pm-pack-and-send">
+        <source>Pack and send</source>
+        <translation>Inpakken en verzenden</translation>
+    </message>
+    <message id="logger-logpage-title">
+        <source>Log</source>
+        <translation>Ofono-log</translation>
+    </message>
+    <message id="logger-logpage-msg-connected">
+        <source>Connected.</source>
+        <translation>Verbonden met ofono.</translation>
+    </message>
+    <message id="logger-logpage-msg-connect-error">
+        <source>We are having trouble connecting to the service.</source>
+        <translation>We ondervinden problemen bij het verbinden met ofono. Mogelijk biedt deze versie van ofono geen ondersteuning voor loggen.</translation>
+    </message>
+    <message id="logger-logpage-msg-disconnected">
+        <source>Connection lost.</source>
+        <translation>Verbinding met ofono verbroken.</translation>
+    </message>
+    <message id="logger-logpage-msg-log-cleared">
+        <source>Log cleared</source>
+        <translation>Log gewist</translation>
+    </message>
+    <message id="logger-categories-title">
+        <source>Log categories</source>
+        <translation>Logcategorieën</translation>
+    </message>
+    <message id="logger-categories-pm-enable-all">
+        <source>Enable all</source>
+        <translation>Alles inschakelen</translation>
+    </message>
+    <message id="logger-categories-pm-disable-all">
+        <source>Disable all</source>
+        <translation>Alles uitschakelen</translation>
+    </message>
+    <message id="logger-categories-pm-default">
+        <source>Reset to default</source>
+        <translation>Standaardwaarden herstellen</translation>
+    </message>
+    <message id="logger-categories-select-placeholder">
+        <source>Select categories</source>
+        <extracomment>Placeholder text for the search field</extracomment>
+        <translation>Selecteer categorieën</translation>
+    </message>
+    <message id="logger-categories-not-connected">
+        <source>Not connected</source>
+        <translation>Niet verbonden met ofono</translation>
+    </message>
+    <message id="logger-sharemethodlist-add-account">
+        <source>Add account</source>
+        <translation>Account toevoegen</translation>
+    </message>
+    <message id="logger-sharepage-save-ok">
+        <source>Saved %1</source>
+        <translation>%1 opgeslagen</translation>
+    </message>
+    <message id="logger-sharepage-save-error">
+        <source>Failed to save %1</source>
+        <translation>Opslaan van %1 mislukt</translation>
+    </message>
+    <message id="logger-sharepage-pm-save-to-documents">
+        <source>Save to documents</source>
+        <translation>Opslaan naar documenten</translation>
+    </message>
+    <message id="logger-sharepage-header">
+        <source>Pack and send</source>
+        <translation>Inpakken en verzenden</translation>
+    </message>
+    <message id="logger-sharepage-default-subject">
+        <source>Log</source>
+        <extracomment>Default email subject</extracomment>
+        <translation>ofono log</translation>
+    </message>
+    <message id="logger-sharepage-default-email">
+        <source></source>
+        <extracomment>Default email recipient</extracomment>
+        <translation>ofono-debug@jolla.com</translation>
+    </message>
+    <message id="logger-sharepage-warning">
+        <source>Keep in mind that some of the information contained in this archive may be considered private. If you would like to check what you are about to send, please consider sending it to yourself and checking its contents first.</source>
+        <translation>Let op: sommige informatie in dit archief is mogelijk privé. Als je wil nakijken wat je gaat verzenden, kan je het eerst naar je eigen e-mailadres verzenden en daar de inhoud controleren.</translation>
+    </message>
+    <message id="logger-sharepage-please-wait">
+        <source>Please wait</source>
+        <translation>Even geduld</translation>
+    </message>
+    <message id="logger-settings-page-header">
+        <source>Logger</source>
+        <extracomment>Page header</extracomment>
+        <translation>Ofono-logger</translation>
+    </message>
+    <message id="logger-settings-section-header-display">
+        <source>Display</source>
+        <extracomment>Section header</extracomment>
+        <translation>Weergave</translation>
+    </message>
+    <message id="logger-settings-logsizelimit">
+        <source>Screen buffer size</source>
+        <translation>Schermbuffergrootte</translation>
+    </message>
+    <message id="logger-settings-logsizelimit-description">
+        <source>Don&apos;t worry, everything will be written to the log file regardless of the screen buffer size.</source>
+        <translation>Geen zorgen, alles wordt naar het logbestand geschreven, onafhankelijk van de schermbuffergrootte.</translation>
+    </message>
+    <message id="logger-settings-logsizelimit-unlimited">
+        <source>Unlimited</source>
+        <translation>Onbeperkt</translation>
+    </message>
+    <message id="logger-settings-fontsize-label">
+        <source>Font size</source>
+        <translation>Lettergrootte</translation>
+    </message>
+    <message id="logger-settings-section-header-logging">
+        <source>Logging</source>
+        <extracomment>Section header</extracomment>
+        <translation>Bezig met loggen</translation>
+    </message>
+    <message id="logger-settings-autoenable_logging-switch-label">
+        <source>Automatically enable logging</source>
+        <extracomment>Text switch label</extracomment>
+        <translation>Loggen automatisch inschakelen</translation>
+    </message>
+    <message id="logger-settings-autoenable_logging-switch-description">
+        <source>Enable all logging categories when the application starts.</source>
+        <extracomment>Text switch description</extracomment>
+        <translation>Schakel alle logcategorieën automatisch in bij opstarten van de toepassing.</translation>
+    </message>
+    <message id="logger-settings-autoreset_logging-switch-label">
+        <source>Automatically reset logging on exit</source>
+        <extracomment>Text switch label</extracomment>
+        <translation>Loggen automatisch herstellen naar standaardwaarden bij afsluiten</translation>
+    </message>
+    <message id="logger-settings-autoreset_logging-switch-description">
+        <source>Reset all logging categories to their default values when the application is exiting. Otherwise logging would continue in the background, wasting the system resources and eating up your battery.</source>
+        <extracomment>Text switch description</extracomment>
+        <translation>Herstel alle logcategorieën naar hun standaardwaarden wanneer de toepassing afgesloten wordt. Zoniet zou het loggen in de achtergrond blijven doorgaan, waardoor je systeembronnen nodeloos gebruikt zouden worden en je batterij sneller plat zou zijn.</translation>
+    </message>
+</context>
+</TS>
