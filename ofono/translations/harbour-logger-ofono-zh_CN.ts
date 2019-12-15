b4d98b9840d68161ee8f1fb00276d7a99671c2c6
dashinfantry <45334556+dashinfantry@users.noreply.github.com>
2019-12-15 18:08:43 +0800

    Create harbour-logger-ofono-zh_CN.ts

---
+++ ofono/translations/harbour-logger-ofono-zh_CN.ts
@@ -0,0 +1,167 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name></name>
+    <message id="logger-cover-title">
+        <source>Log</source>
+        <translation>日志</translation>
+    </message>
+    <message id="logger-mainpage-swipe-left-hint">
+        <source>Swipe left to configure log categories</source>
+        <translation>左滑以配置日志分类</translation>
+    </message>
+    <message id="ofono-logger-pm-fix-mobile-data">
+        <source>Fix mobile data</source>
+        <extracomment>Pull-down menu item</extracomment>
+        <translation>修复移动数据</translation>
+    </message>
+    <message id="ofono-logger-pm-enable-mobile-data">
+        <source>Enable mobile data</source>
+        <extracomment>Pull-down menu item</extracomment>
+        <translation>打开移动数据</translation>
+    </message>
+    <message id="logger-logpage-pm-clear-log">
+        <source>Clear log</source>
+        <translation>清空日志</translation>
+    </message>
+    <message id="logger-logpage-pm-pack-and-send">
+        <source>Pack and send</source>
+        <translation>打包及发送</translation>
+    </message>
+    <message id="logger-logpage-title">
+        <source>Log</source>
+        <translation>Ofono log</translation>
+    </message>
+    <message id="logger-logpage-msg-connected">
+        <source>Connected.</source>
+        <translation>已连接。</translation>
+    </message>
+    <message id="logger-logpage-msg-connect-error">
+        <source>We are having trouble connecting to the service.</source>
+        <translation>连接服务出错。</translation>
+    </message>
+    <message id="logger-logpage-msg-disconnected">
+        <source>Connection lost.</source>
+        <translation>连接丢失。</translation>
+    </message>
+    <message id="logger-logpage-msg-log-cleared">
+        <source>Log cleared</source>
+        <translation>已清除日志</translation>
+    </message>
+    <message id="logger-categories-title">
+        <source>Log categories</source>
+        <translation>日志分类</translation>
+    </message>
+    <message id="logger-categories-pm-enable-all">
+        <source>Enable all</source>
+        <translation>开启全部</translation>
+    </message>
+    <message id="logger-categories-pm-disable-all">
+        <source>Disable all</source>
+        <translation>禁用全部</translation>
+    </message>
+    <message id="logger-categories-pm-default">
+        <source>Reset to default</source>
+        <translation>重置为默认</translation>
+    </message>
+    <message id="logger-categories-select-placeholder">
+        <source>Select categories</source>
+        <extracomment>Placeholder text for the search field</extracomment>
+        <translation>选择分类</translation>
+    </message>
+    <message id="logger-categories-not-connected">
+        <source>Not connected</source>
+        <translation>未连接</translation>
+    </message>
+    <message id="logger-sharemethodlist-add-account">
+        <source>Add account</source>
+        <translation>添加账户</translation>
+    </message>
+    <message id="logger-sharepage-save-ok">
+        <source>Saved %1</source>
+        <translation>已保存 %1</translation>
+    </message>
+    <message id="logger-sharepage-save-error">
+        <source>Failed to save %1</source>
+        <translation>保存 %1 失败</translation>
+    </message>
+    <message id="logger-sharepage-pm-save-to-documents">
+        <source>Save to documents</source>
+        <translation>保存到文档</translation>
+    </message>
+    <message id="logger-sharepage-header">
+        <source>Pack and send</source>
+        <translation>打包且发送</translation>
+    </message>
+    <message id="logger-sharepage-default-subject">
+        <source>Log</source>
+        <extracomment>Default email subject</extracomment>
+        <translation>日志</translation>
+    </message>
+    <message id="logger-sharepage-default-email">
+        <source></source>
+        <extracomment>Default email recipient</extracomment>
+        <translation>ofono-debug@jolla.com</translation>
+    </message>
+    <message id="logger-sharepage-warning">
+        <source>Keep in mind that some of the information contained in this archive may be considered private. If you would like to check what you are about to send, please consider sending it to yourself and checking its contents first.</source>
+        <translation>请记住，此档案中包含某些可能被视为私人内容的信息。 如果你想查看将要发送的内容，请考虑先将其发送给自己并检查其内容。</translation>
+    </message>
+    <message id="logger-sharepage-please-wait">
+        <source>Please wait</source>
+        <translation>请稍等</translation>
+    </message>
+    <message id="logger-settings-page-header">
+        <source>Logger</source>
+        <extracomment>Page header</extracomment>
+        <translation>日志生成器</translation>
+    </message>
+    <message id="logger-settings-section-header-display">
+        <source>Display</source>
+        <extracomment>Section header</extracomment>
+        <translation>显示</translation>
+    </message>
+    <message id="logger-settings-logsizelimit">
+        <source>Screen buffer size</source>
+        <translation>屏幕缓冲区大小</translation>
+    </message>
+    <message id="logger-settings-logsizelimit-description">
+        <source>Don&apos;t worry, everything will be written to the log file regardless of the screen buffer size.</source>
+        <translation>别担心，所有信息都会被写进日志文件，你不必担心屏幕缓冲区大小。</translation>
+    </message>
+    <message id="logger-settings-logsizelimit-unlimited">
+        <source>Unlimited</source>
+        <translation>无限制</translation>
+    </message>
+    <message id="logger-settings-fontsize-label">
+        <source>Font size</source>
+        <translation>字体大小</translation>
+    </message>
+    <message id="logger-settings-section-header-logging">
+        <source>Logging</source>
+        <extracomment>Section header</extracomment>
+        <translation>正在缓冲</translation>
+    </message>
+    <message id="logger-settings-autoenable_logging-switch-label">
+        <source>Automatically enable logging</source>
+        <extracomment>Text switch label</extracomment>
+        <translation>自动开始缓冲</translation>
+    </message>
+    <message id="logger-settings-autoenable_logging-switch-description">
+        <source>Enable all logging categories when the application starts.</source>
+        <extracomment>Text switch description</extracomment>
+        <translation>软件打开之后自动开启全部缓冲分类。</translation>
+    </message>
+    <message id="logger-settings-autoreset_logging-switch-label">
+        <source>Automatically reset logging on exit</source>
+        <extracomment>Text switch label</extracomment>
+        <translation>退出软件时自动重置日志。</translation>
+    </message>
+    <message id="logger-settings-autoreset_logging-switch-description">
+        <source>Reset all logging categories to their default values when the application is exiting. Otherwise logging would continue in the background, wasting the system resources and eating up your battery.</source>
+        <extracomment>Text switch description</extracomment>
+        <translation>退出软件时重置全部日志分类为默认值。否则软件会在后台继续记录日志，浪费系统资源并且消耗你的电量。</translation>
+    </message>
+</context>
+</TS>
